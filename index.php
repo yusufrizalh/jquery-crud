98a4d2f760e00f76fcee244416ce9e239f5c2261
Yusuf Rizal <yusufrizalh@gmail.com>
2019-10-01 11:56:52 +0700

    belajar jquery menambah index dan ajax

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,8 +14,10 @@
     <title>CRUD with jQuery AJAX</title>
     <!-- import CSS dan Bootstrap -->
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
+
 <body>
     <div class="container">
         <div class="row">
@@ -23,10 +26,9 @@
                 <hr class="ma-hr">
                 <!-- membuat tombol tambah data -->
                 <div class="float-right">
-                    <a href="javascript:void(0);" class="btn btn-success" 
-                        data-type="add" data-toggle="modal" 
+                    <a href="javascript:void(0);" class="btn btn-success" data-type="add" data-toggle="modal"
                         data-targe="#modalUserAddEdit">
-                    <i class="plus">Buat User Baru</i>
+                        <i class="plus">Buat User Baru</i>
                     </a>
                 </div>
             </div>
@@ -69,5 +71,67 @@
             </table>
         </div>
     </div>
+
+    <!-- membuat form untuk menambah dan mengubah data -->
+    <div class="modal fade" id="modalUserAddEdit" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <!-- modal header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Menambah User Baru</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+
+                <!-- modal body -->
+                <div class="modal-body">
+                    <div class="statusMsg"></div>
+                    <form role="form">
+                        <div class="form-group">
+                            <label for="name">Nama</label>
+                            <input type="text" class="form-control" name="name" id="name" 
+                                placeholder="Tuliskan nama">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" name="email" id="email"
+                                placeholder="Tuliskan email">
+                        </div>
+                        <div class="form-group">
+                            <label for="phone">Telepon</label>
+                            <input type="text" class="form-control" name="phone" id="phone"
+                                placeholder="Tuliskan telepon">
+                        </div>
+                        <input type="hidden" class="form-control" name="id" id="id" />
+                    </form>
+                </div>
+
+                <!-- modal footer -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Tutup</button>
+                    <button type="button" class="btn btn-primary" id="userSubmit">Simpan</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- import jQuery dan dataTables -->
+    <script src="./js/jquery-3.4.1.min.js"></script>
+    <script src="./js/jquery.dataTables.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script>
+        function getUsers() {
+            $.ajax({
+                type: 'POST', 
+                url: 'userAction.php', 
+                data: 'action_type=view', 
+                success: function(html) {
+                    $('#userData').html(html);
+                }
+            });
+        }
+
+        
+    </script>
 </body>
+
 </html>
